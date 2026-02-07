ecc0c3ebd27cf62942cf99f70bb294d063f74547
Luka Lončar <lloncar.vz@gmail.com>
2026-02-07 16:02:59 +0200

    added Singleton classes

---
+++ Assets/Scripts/Utils/PersistentSingleton.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace LastNightsMasks.Utils {
+    public class PersistentSingleton<T> : MonoBehaviour where T : Component {
+        private static T _instance;
+        public static bool HasInstance =>  _instance != null;
+
+        public static T Instance {
+            get {
+                if (_instance == null) {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null) {
+                        GameObject go = new GameObject(typeof(T).Name);
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake() {
+            if (!Application.isPlaying)
+                return;
+
+            if (_instance != null && _instance != this) {
+                Destroy(gameObject);
+                return;
+            }
+            
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        protected virtual void OnDestroy() {
+            if (_instance == this)
+                _instance = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace LastNightsMasks.Utils {
+    public class Singleton<T> : MonoBehaviour where T : Component {
+        private static T _instance;
+        public static bool HasInstance =>  _instance != null;
+        
+        public static T Instance {
+            get {
+                if (_instance == null) {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null) {
+                        GameObject go = new GameObject(typeof(T).Name + " Auto-Generated");
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+        
+        protected virtual void Awake() {
+            if (!Application.isPlaying) return;
+            
+            if (_instance != null && _instance != this) {
+                Destroy(gameObject);
+                return;
+            }
+            
+            _instance = this as T;
+        }
+        
+        protected virtual void OnDestroy() {
+            if (_instance == this)
+                _instance = null;
+        }
+    }
+}
